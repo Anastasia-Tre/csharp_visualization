de299c45e3d0475bd76fd0a336523a6a68d67ff5
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-04 15:21:57 +0200

    connect StandartGraphics to GraphicsWPF

--- BasicGraphics/GraphicsWPF/MainWindow.xaml.cs
+++ BasicGraphics/GraphicsWPF/MainWindow.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GraphicsWPF
 {
@@ -26,27 +23,26 @@ namespace GraphicsWPF
         private void Render()
         {
             Random rnd = new Random();
-            Bitmap bmp = StandardGraphics.Render.GetBitmap(rnd, (int)ResultCanvas.ActualWidth, (int)ResultCanvas.ActualHeight);
+            using Bitmap bmp = new Bitmap((int)ResultCanvas.ActualWidth, (int)ResultCanvas.ActualHeight);
+            StandardGraphics.Render.GenerateImage(bmp, rnd, 500);
             ResultImage.Source = BitmapToBitmapImage(bmp);
         }
 
         private BitmapImage BitmapToBitmapImage(Bitmap bmp)
         {
-            using (var memory = new System.IO.MemoryStream())
-            {
-                bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
-                memory.Position = 0;
+            using var memory = new System.IO.MemoryStream();
+            bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+            memory.Position = 0;
 
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memory;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
 
-                return bitmapImage;
-            }
-            
+            return bitmapImage;
         }
+
     }
 }
--- BasicGraphics/StandardGraphics/Render.cs
+++ BasicGraphics/StandardGraphics/Render.cs
@@ -5,26 +5,23 @@ namespace StandardGraphics
 {
     public static class Render
     {
-        public static Bitmap GetBitmap(Random rnd, int width, int height, int circles = 100)
+        public static void GenerateImage(Bitmap bmp, Random rnd, int circles = 100)
         {
-            using Bitmap bmp = new Bitmap(width, height);
-            using (Graphics gfx = Graphics.FromImage(bmp))
-            using (Pen pen = new Pen(Color.White))
+            using Graphics gfx = Graphics.FromImage(bmp);
+            using Pen pen = new Pen(Color.White);
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            gfx.Clear(Color.White);
+            for (int i = 0; i < circles; i++)
             {
-                gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-                gfx.Clear(Color.Black);
-                for (int i = 0; i < circles; i++)
-                {
-                    pen.Color = Color.FromArgb(rnd.Next());
+                pen.Color = Color.FromArgb(rnd.Next());
 
-                    float x = rnd.Next(bmp.Width);
-                    float y = rnd.Next(bmp.Height);
-                    float size = rnd.Next(10);
+                float x = rnd.Next(bmp.Width);
+                float y = rnd.Next(bmp.Height);
+                float size = rnd.Next(30);
 
-                    gfx.DrawEllipse(pen, x, y, size, size);
-                }
+                gfx.DrawEllipse(pen, x, y, size, size);
             }
-            return bmp;
+
         }
     }
 }
