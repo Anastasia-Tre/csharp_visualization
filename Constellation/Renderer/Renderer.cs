9b01a2a0012ca3e55a49adad98f2883d9aa2bbb6
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-09 15:00:24 +0200

    create class for renderer

---
+++ Constellation/Renderer/Renderer.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer
+{
+    public class Renderer : IRenderer
+    {
+        public void Clear(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawLine(Point pt1, Point pt2, double lineWidth, Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FillCircle(Point center, double radius, Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
