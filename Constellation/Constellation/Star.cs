8c8d1a47c4fd8f28a9041236099a42c7ee324005
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-02-09 14:07:55 +0200

    add class for star

---
+++ Constellation/Constellation/Star.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Star
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Vel { get; set; }
+
+        public float Size { get; set; } = 3;
+        public Color Color { get; set; } = new Color(255, 255, 255);
+
+    }
+}
